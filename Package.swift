3b355a5ba6f40c71ce1676d178f94c223021f014
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2020-07-08 12:12:28 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,22 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "VD",
+    name: "VDKit",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "VD",
-            targets: ["VD"]),
+            name: "VDKit",
+            targets: ["VDKit"]),
     ],
     dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "VD",
+            name: "VDKit",
             dependencies: []),
         .testTarget(
             name: "VDTests",
-            dependencies: ["VD"]),
+            dependencies: ["VDKit"]),
     ]
 )
