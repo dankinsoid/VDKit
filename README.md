78b8028c31165b12e3e2d913326aec3016e0d379
Danil <voidilov@gmail.com>
2021-02-10 17:35:40 +0300

    1.0.46

--- README.md
+++ README.md
@@ -88,7 +88,7 @@ import PackageDescription
 let package = Package(
   name: "SomeProject",
   dependencies: [
-    .package(url: "https://github.com/dankinsoid/VDKit.git", from: "1.0.45")
+    .package(url: "https://github.com/dankinsoid/VDKit.git", from: "1.0.46")
   ],
   targets: [
     .target(name: "SomeProject", dependencies: ["VDKit"])
--- Sources/VDKit/Chaining.swift
+++ Sources/VDKit/Chaining.swift
@@ -45,8 +45,16 @@ public struct TypeChaining<W>: Chaining {
 		self.action = { $0 }
 	}
 	
-	public subscript<A>(dynamicMember keyPath: WritableKeyPath<W, A>) -> ChainingProperty<Self, A> {
-		ChainingProperty<Self, A>(self, keyPath: keyPath)
+	public subscript<A>(dynamicMember keyPath: KeyPath<W, A>) -> ChainingProperty<Self, A, KeyPath<W, A>> {
+		ChainingProperty<Self, A, KeyPath<W, A>>(self, getter: keyPath)
+	}
+	
+	public subscript<A>(dynamicMember keyPath: WritableKeyPath<W, A>) -> ChainingProperty<Self, A, WritableKeyPath<W, A>> {
+		ChainingProperty<Self, A, WritableKeyPath<W, A>>(self, getter: keyPath)
+	}
+	
+	public subscript<A>(dynamicMember keyPath: ReferenceWritableKeyPath<W, A>) -> ChainingProperty<Self, A, ReferenceWritableKeyPath<W, A>> {
+		ChainingProperty<Self, A, ReferenceWritableKeyPath<W, A>>(self, getter: keyPath)
 	}
 	
 	public func apply(for values: W...) {
@@ -74,8 +82,16 @@ public struct ValueChaining<W>: ValueChainingProtocol {
 		wrappedValue = value
 	}
 	
-	public subscript<A>(dynamicMember keyPath: WritableKeyPath<W, A>) -> ChainingProperty<Self, A> {
-		ChainingProperty<Self, A>(self, keyPath: keyPath)
+	public subscript<A>(dynamicMember keyPath: KeyPath<W, A>) -> ChainingProperty<Self, A, KeyPath<W, A>> {
+		ChainingProperty<Self, A, KeyPath<W, A>>(self, getter: keyPath)
+	}
+	
+	public subscript<A>(dynamicMember keyPath: WritableKeyPath<W, A>) -> ChainingProperty<Self, A, WritableKeyPath<W, A>> {
+		ChainingProperty<Self, A, WritableKeyPath<W, A>>(self, getter: keyPath)
+	}
+	
+	public subscript<A>(dynamicMember keyPath: ReferenceWritableKeyPath<W, A>) -> ChainingProperty<Self, A, ReferenceWritableKeyPath<W, A>> {
+		ChainingProperty<Self, A, ReferenceWritableKeyPath<W, A>>(self, getter: keyPath)
 	}
 	
 	public func copy(with action: @escaping (W) -> W) -> ValueChaining<W> {
@@ -86,52 +102,99 @@ public struct ValueChaining<W>: ValueChainingProtocol {
 	
 }
 
-@dynamicMemberLookup
-public struct ChainingProperty<C: Chaining, P> {
-	public let chaining: C
-	public let setter: (C.W, P) -> C.W
-	public let getter: (C.W) -> P
-	
-	public init(_ value: C, setter: @escaping (C.W, P) -> C.W,  getter: @escaping (C.W) -> P) {
-		chaining = value
-		self.setter = setter
-		self.getter = getter
+public protocol GetterProtocol {
+	associatedtype A
+	associatedtype B
+	var get: (A) -> B { get }
+}
+
+public protocol SetterProtocol: GetterProtocol {
+	var set: (A, B) -> A { get }
+}
+
+public struct Getter<A, B>: GetterProtocol {
+	public let get: (A) -> B
+}
+
+public struct Setter<A, B>: SetterProtocol {
+	public let get: (A) -> B
+	public let set: (A, B) -> A
+}
+
+extension KeyPath: GetterProtocol {
+	public var get: (Root) -> Value {
+		{ $0[keyPath: self] }
 	}
-	
-	public init(_ value: C, keyPath: WritableKeyPath<C.W, P>) {
-		chaining = value
-		self.setter = {
+}
+
+extension WritableKeyPath: SetterProtocol {
+	public var set: (Root, Value) -> Root {
+		{
 			var result = $0
-			result[keyPath: keyPath] = $1
+			result[keyPath: self] = $1
 			return result
 		}
-		self.getter = {
-			$0[keyPath: keyPath]
-		}
+	}
+}
+
+@dynamicMemberLookup
+public struct ChainingProperty<C: Chaining, B, G: GetterProtocol> where G.A == C.W, G.B == B {
+	public let chaining: C
+	public let getter: G
+	
+	public init(_ value: C, getter: G) {
+		chaining = value
+		self.getter = getter
+	}
+
+}
+
+extension ChainingProperty where G: KeyPath<C.W, B> {
+	
+	public subscript<A>(dynamicMember keyPath: KeyPath<G.B, A>) -> ChainingProperty<C, A, KeyPath<C.W, A>> {
+		ChainingProperty<C, A, KeyPath<C.W, A>>(chaining, getter: getter.appending(path: keyPath))
 	}
 	
-	public subscript<A>(dynamicMember keyPath: WritableKeyPath<P, A>) -> ChainingProperty<C, A> {
-		ChainingProperty<C, A>(chaining, setter: {
-			var value = getter($0)
-			value[keyPath: keyPath] = $1
-			return setter($0, value)
-		}, getter: {
-			getter($0)[keyPath: keyPath]
-		}
-		)
+	public subscript<A>(dynamicMember keyPath: ReferenceWritableKeyPath<G.B, A>) -> ChainingProperty<C, A, ReferenceWritableKeyPath<C.W, A>> {
+		ChainingProperty<C, A, ReferenceWritableKeyPath<C.W, A>>(chaining, getter: getter.append(reference: keyPath))
 	}
 	
-	public subscript(_ value: P) -> C {
+}
+
+
+extension ChainingProperty where G: WritableKeyPath<C.W, B> {
+	
+	public subscript<A>(dynamicMember keyPath: WritableKeyPath<G.B, A>) -> ChainingProperty<C, A, WritableKeyPath<C.W, A>> {
+		ChainingProperty<C, A, WritableKeyPath<C.W, A>>(chaining, getter: getter.append(keyPath))
+	}
+	
+}
+
+extension KeyPath {
+	func append<A>(reference: ReferenceWritableKeyPath<Value, A>) -> ReferenceWritableKeyPath<Root, A> {
+		appending(path: reference)
+	}
+}
+
+extension WritableKeyPath {
+	func append<A>(_ path: WritableKeyPath<Value, A>) -> WritableKeyPath<Root, A> {
+		appending(path: path)
+	}
+}
+
+extension ChainingProperty where G: SetterProtocol {
+	
+	public subscript(_ value: G.B) -> C {
 		chaining.copy {
-			setter(chaining.action($0), value)
+			getter.set(chaining.action($0), value)
 		}
 	}
 	
 }
 
-extension ChainingProperty where C: ValueChainingProtocol {
+extension ChainingProperty where C: ValueChainingProtocol, G: SetterProtocol {
 	
-	public subscript(_ value: P) -> C.W {
+	public subscript(_ value: G.B) -> C.W {
 		self[value].apply()
 	}
 	
