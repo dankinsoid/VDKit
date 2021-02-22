7f8f51254526c02876d531c72758e53c38f76725
Danil <voidilov@gmail.com>
2021-02-22 13:18:39 +0300

    1.10.0

--- Sources/VDKit/ComposeBuilder.swift
+++ Sources/VDKit/ComposeBuilder.swift
@@ -15,27 +15,33 @@ public protocol ArrayInitable {
 @_functionBuilder
 public struct ComposeBuilder<C: ArrayInitable> {
 	
-	static func buildBlock(_ components: C.Item...) -> C.Item {
+	@inlinable
+	public static func buildBlock(_ components: C.Item...) -> C.Item {
 		C.create(from: components)
 	}
 	
-	static func buildArray(_ components: [C.Item]) -> C.Item {
+	@inlinable
+	public static func buildArray(_ components: [C.Item]) -> C.Item {
 		C.create(from: components)
 	}
 	
-	static func buildEither(first component: C.Item) -> C.Item {
+	@inlinable
+	public static func buildEither(first component: C.Item) -> C.Item {
 		component
 	}
 	
-	static func buildEither(second component: C.Item) -> C.Item {
+	@inlinable
+	public static func buildEither(second component: C.Item) -> C.Item {
 		component
 	}
 	
-	static func buildOptional(_ component: C.Item?) -> C.Item {
+	@inlinable
+	public static func buildOptional(_ component: C.Item?) -> C.Item {
 		component ?? C.create(from: [])
 	}
 	
-	static func buildLimitedAvailability(_ component: C.Item) -> C.Item {
+	@inlinable
+	public static func buildLimitedAvailability(_ component: C.Item) -> C.Item {
 		component
 	}
 	
