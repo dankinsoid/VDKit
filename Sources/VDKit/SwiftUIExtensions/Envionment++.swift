093ca94fb70202a4a23a57c21280cec20fd5cb84
Danil <voidilov@gmail.com>
2021-05-16 00:52:21 +0300

    1.47.0

---
+++ Sources/VDKit/SwiftUIExtensions/Envionment++.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Данил Войдилов on 16.05.2021.
+//
+
+import SwiftUI
+
+private struct TypeKey<T>: Hashable {}
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+private extension EnvironmentValues {
+	subscript<T: EnvironmentKey>(type: TypeKey<T>) -> T.Value {
+		get { self[T.self] }
+		set { self[T.self] = newValue }
+	}
+}
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+extension View {
+	
+	public func environment<Key: EnvironmentKey>(_ key: Key.Type, _ value: Key.Value) -> some View {
+		environment(\.[TypeKey<Key>()], value)
+	}
+}
