3cee6d6380bbde022da2a7d4de758393bc9f3ba1
Danil <voidilov@gmail.com>
2021-05-20 19:09:50 +0300

    1.55.0

--- Sources/VDKit/SwiftUIExtensions/BackdropView.swift
+++ Sources/VDKit/SwiftUIExtensions/BackdropView.swift
@@ -10,9 +10,28 @@ import SwiftUI
 @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 public struct BackdropView: UIViewRepresentable {
   
+	public init()
+	
 	public func makeUIView(context: Context) -> UIBackdropView {
 		UIBackdropView()
 	}
 	
 	public func updateUIView(_ uiView: UIBackdropView, context: Context) {}
+}
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+public struct Backdrop<Content: View>: View {
+	
+	public let content: Content
+	
+	public init(@ViewBuilder content: () -> Content) {
+		self.content = content()
+	}
+	
+	public var body: some View {
+		ZStack {
+			BackdropView()
+			content
+		}
+	}
 }
