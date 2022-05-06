e83faf9b8149175191112e563bf7589154d82833
dankinsoid <voidilov@gmail.com>
2022-05-06 17:28:35 +0300

    1.190.0

--- Sources/UIKitComposable/SubviewsBuilder.swift
+++ Sources/UIKitComposable/SubviewsBuilder.swift
@@ -10,12 +10,46 @@
 import UIKit
 import VDBuilders
 
-public typealias SubviewsBuilder = ArrayBuilder<SubviewProtocol>
-
-extension ArrayBuilder where T == SubviewProtocol {
+@resultBuilder
+public struct SubviewsBuilder {
+	
+	@inlinable
+	public static func buildBlock(_ components: [SubviewProtocol]...) -> [SubviewProtocol] {
+		Array(components.joined())
+	}
+	
+	@inlinable
+	public static func buildArray(_ components: [[SubviewProtocol]]) -> [SubviewProtocol] {
+		Array(components.joined())
+	}
+	
+	@inlinable
+	public static func buildEither(first component: [SubviewProtocol]) -> [SubviewProtocol] {
+		component
+	}
+	
+	@inlinable
+	public static func buildEither(second component: [SubviewProtocol]) -> [SubviewProtocol] {
+		component
+	}
+	
+	@inlinable
+	public static func buildOptional(_ component: [SubviewProtocol]?) -> [SubviewProtocol] {
+		component ?? []
+	}
+	
+	@inlinable
+	public static func buildLimitedAvailability(_ component: [SubviewProtocol]) -> [SubviewProtocol] {
+		component
+	}
+	
+	@inlinable
+	public static func buildExpression(_ expression: SubviewProtocol) -> [SubviewProtocol] {
+		[expression]
+	}
 	
 	@inline(__always)
-	public static func buildExpression<S: SubviewProtocol>(_ expression: S) -> [T] {
+	public static func buildExpression<S: SubviewProtocol>(_ expression: S) -> [SubviewProtocol] {
 		[expression]
 	}
 }
@@ -23,14 +57,13 @@ extension ArrayBuilder where T == SubviewProtocol {
 #if canImport(SwiftUI)
 import SwiftUI
 
-extension ArrayBuilder where T == SubviewProtocol {
+extension SubviewsBuilder {
 	
 	@available(iOS 13.0, *)
 	@inline(__always)
-	public static func buildExpression<S: View>(_ expression: S) -> [T] {
+	public static func buildExpression<S: View>(_ expression: S) -> [SubviewProtocol] {
 		[UIHostingController(rootView: expression)]
 	}
 }
-
 #endif
 #endif
