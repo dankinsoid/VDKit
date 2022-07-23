1e299635cff81cb95286df60003517aedaf0a50b
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2022-07-24 00:37:45 +0300

    1.191.0

--- Sources/SwiftUI/Field/UIField.swift
+++ Sources/SwiftUI/Field/UIField.swift
@@ -86,7 +86,7 @@ open class UIField<Input: View>: UITextField, UITextFieldDelegate {
 	open func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 		let newText = ((text ?? "") as NSString).replacingCharacters(in: range, with: string)
 		onChange(newText)
-		return range.length == 0
+		return false
 	}
 	
 	open func textFieldDidChangeSelection(_ textField: UITextField) {
